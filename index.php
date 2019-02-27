1f57934d76cff6aca76d3e5b8bf581580715530e
synsharjeel <37326681+synsharjeel@users.noreply.github.com>
2019-02-27 12:42:06 +0500

    intial commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php  
+ //entry.php  
+ session_start();  
+ if(!isset($_SESSION["username"]))  
+ {  
+      header("location:login.php?action=login");  
+ }  
+ ?>  
+
+<html>  
+      <head>  
+     
+</head>
+
+<body class="">
+  <div class="wrapper ">
+   
+  <?php $page="index";
+  
+  ?>
+
+
+           <title>PHPExcel - Export Excel file into Mysql Database using Ajax</title>
+             
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+           <style>  
+                body  
+                {  
+                     margin:0;  
+                     padding:0;  
+                     background-color:#f1f1f1;  
+                }  
+                .box  
+                {  
+                     width:900px;  
+                     padding:20px;  
+                     background-color:#fff;  
+                     border:1px solid #ccc;  
+                     border-radius:5px;  
+                     margin-top:100px;  
+                }  
+           </style>  
+      </head>  
+      
+      <body>  
+           <div class="container box">  
+                <h3 align="center"> Export Excel file</h3>  
+                <br /><br />  
+                <br /><br />  
+
+               
+                <form mehtod="post" id="export_excel">  
+                     <label>Select Excel</label>  
+                     <input type="file" name="excel_file" id="excel_file" />  <br>
+
+
+                     <input type="submit" class="btn btn-primary" value="Uplaod" />
+                </form>  
+                <br />  
+                <br />  
+                <div id="result">  
+                </div>  
+           </div>  
+      </body>  
+ </html>  
+ <script>  
+ $(document).ready(function(){  
+      $('#excel_file').change(function(){  
+          //  $('#export_excel').submit();  
+      });  
+      $('#export_excel').on('submit', function(event){  
+           event.preventDefault();  
+           $.ajax({  
+                url:"export.php",  
+                method:"POST",  
+                data:new FormData(this),  
+                contentType:false,  
+                processData:false,  
+                success:function(data){  
+                     $('#result').html(data);  
+                     $('#excel_file').val('');  
+                }  
+           });  
+      });  
+ });  
+ </script>  
+
+                <?php  
+               //  echo '<h1>Welcome - '.$_SESSION["username"].'</h1>';  
+                echo '<label align="center"><a href="logout.php">Logout</a></label>';  
+                ?>  
